9ca97e8eb249988d829ed3c55ef66a4794407e9f
dimaBel1 <dima.chunosov86@mail.ru>
2023-08-18 17:01:55 +0300

    add accordion-1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-const swiper = new Swiper('.swiper-reviews', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 2,
-    spaceBetween: 30,
+function initAccordeon() {
+  const firstSectionBodyHeight = document.querySelector(
+    ".accordeon-section .accordeon-body > *"
+  ).clientHeight;
+  document.querySelector(".accordeon-section .accordeon-body").style.maxHeight =
+    firstSectionBodyHeight + "px";
+}
+initAccordeon();
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-
+const accordeonHeaderClickHandler = (e) => {
+  document.querySelectorAll(".accordeon-section").forEach(function (section) {
+    section.querySelector(".accordeon-body").style.maxHeight = "0px";
   });
+
+  const accordeonSection = e.target.closest(".accordeon-section");
+
+  const insideElHeight = accordeonSection.querySelector(
+    ".accordeon-body > *"
+  ).clientHeight;
+
+  accordeonSection.querySelector(".accordeon-body").style.maxHeight =
+    insideElHeight + "px";
+};
+
+document.querySelectorAll(".accordeon-section").forEach(function (section) {
+  section.addEventListener("click", accordeonHeaderClickHandler);
+});
+
+const swiper = new Swiper(".swiper-reviews", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
