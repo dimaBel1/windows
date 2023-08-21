da3be3e773458bbff0971cbd2ce8ea69800409d2
dimaBel1 <dima.chunosov86@mail.ru>
2023-08-21 16:11:27 +0300

    add hover

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ function initAccordeon() {
   document.querySelector(".accordeon-section .accordeon-body").style.maxHeight =
     firstSectionBodyHeight + "px";
 
-    document.querySelectorAll('.accordion-arrow')[0].style.transform = "rotate(0deg)";
+  document.querySelectorAll(".accordion-arrow")[0].style.transform =
+    "rotate(0deg)";
 }
 initAccordeon();
 
@@ -34,6 +35,48 @@ const accordeonHeaderClickHandler = (e) => {
 document.querySelectorAll(".accordeon-section").forEach(function (section) {
   section.addEventListener("click", accordeonHeaderClickHandler);
 });
+
+
+function initAccordeonTwo() {
+  const firstSectionBodyHeight = document.querySelector(
+    ".accordeon-section__two .accordeon-body__two > *"
+  ).clientHeight;
+  document.querySelector(
+    ".accordeon-section__two .accordeon-body__two"
+  ).style.maxHeight = firstSectionBodyHeight + "px";
+  document.querySelectorAll(".accordion-arrow__two")[0].style.transform =
+  "rotate(0deg)";
+}
+
+initAccordeonTwo();
+
+const accordeonHeaderClickHandlerTwo = function (e) {
+  document
+    .querySelectorAll(".accordeon-section__two")
+    .forEach(function (section) {
+      section.querySelector(".accordeon-body__two").style.maxHeight = "0px";
+      section.querySelectorAll(".accordion-arrow__two").forEach((itemImg) => {
+        itemImg.style.transform = "rotate(180deg)";
+      });
+    });
+
+  const accordeonSection = e.target.closest(".accordeon-section__two");
+
+  const insideElHeight = accordeonSection.querySelector(
+    ".accordeon-body__two > *"
+  ).clientHeight;
+
+  accordeonSection.querySelector(".accordeon-body__two").style.maxHeight =
+    insideElHeight + "px";
+    accordeonSection.querySelector(".accordion-arrow__two").style.transform =
+    "rotate(0deg)";
+};
+
+document
+  .querySelectorAll(".accordeon-section__two")
+  .forEach(function (section) {
+    section.addEventListener("click", accordeonHeaderClickHandlerTwo);
+  });
 
 const swiper = new Swiper(".swiper-reviews", {
   loop: true,
